2bc03988f819cb802df0bedc739cb04237acd95c
daspro-obsheet2 <wildanwibisono52@gmail.com>
2024-10-25 10:08:07 +0700

    UTS NO 2

--- ManajemenBarang14.java
+++ ManajemenBarang14.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class ManajemenBarang14 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int jumlahStok;
+        double hargaUnit, totalStok;
         System.out.println("Manajemen Stok Barang");
         
         while (true) {
@@ -16,19 +17,19 @@ public class ManajemenBarang14 {
             }
             
             System.out.print("Masukkan jumlah stok " + namaBarang + ": ");
-            int jumlahStok = scanner.nextInt();
+            jumlahStok = scanner.nextInt();
             
             System.out.print("Masukkan harga per unit " + namaBarang + ": ");
-            double hargaPerUnit = scanner.nextDouble();
+            hargaUnit = scanner.nextDouble();
             
-            double totalNilaiStok = jumlahStok * hargaPerUnit;
+            totalStok = jumlahStok * hargaUnit;
             scanner.nextLine();
             
             System.out.println("\nInformasi Barang:");
             System.out.println("Nama Barang: " + namaBarang);
             System.out.println("Stok Tersedia: " + jumlahStok);
-            System.out.println("Harga Per Unit: Rp " + hargaPerUnit);
-            System.out.println("Total Nilai Stok: Rp " +  totalNilaiStok);
+            System.out.println("Harga Per Unit: Rp " + hargaUnit);
+            System.out.println("Total Nilai Stok: Rp " +  totalStok);
             
             System.out.print("\nMasukkan jumlah permintaan " + namaBarang + ": ");
             int jumlahPermintaan = scanner.nextInt();    
@@ -36,7 +37,7 @@ public class ManajemenBarang14 {
        
             if (jumlahPermintaan <= jumlahStok) {
                 int stokSetelahPermintaan = jumlahStok - jumlahPermintaan;
-                double totalNilaiSetelahPermintaan = stokSetelahPermintaan * hargaPerUnit;
+                double totalNilaiSetelahPermintaan = stokSetelahPermintaan * hargaUnit;
                 
                 System.out.println("\nHasil Permintaan: Permintaan dapat dipenuhi");
                 System.out.println("Jumlah yang diminta: " + jumlahPermintaan);
